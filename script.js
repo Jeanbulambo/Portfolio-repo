f372f51d8c4db924fcda83e2822b1f871c8dd6b1
Jean Bulambo <jeanbulambo4@gmail.com>
2022-06-21 23:44:30 +0200

    creat script.js file

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Beginning of Mobile Menu 
 const nav = document.querySelector('.mobile');
 const hamb = document.querySelector('.menu');
 
-hamb.addEventListener('click' , () => {
-hamb.classList.toggle('active');
-nav.classList.toggle('active');
-})
+hamb.addEventListener('click', () => {
+  hamb.classList.toggle('active');
+  nav.classList.toggle('active');
+});
